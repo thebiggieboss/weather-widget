a83109f91f40212756b48d141a9d5b5648ca6ef1
thebiggieboss <thebigboss@internet.ru>
2023-08-06 23:32:08 +0600

    init

---
+++ src/service/base.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// const BaseUrl = import.meta.env.VITE_API_URL;
+
+// const Provider = axios.create({
+//     headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//     },
+//     baseURL: `${BaseUrl}/api/`,
+//     timeout: 100000,
+// });
+//
+// export { Provider, BaseUrl };
+// https://api.openweathermap.org/data/2.5/weather?q=Almaty&units=metric
